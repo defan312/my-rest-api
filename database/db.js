241ceab9a245e698c8596ac08a981161d33269ee
defan312 <106823339+defan312@users.noreply.github.com>
2023-03-02 01:40:36 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,1 +1,1 @@
-mongodb+srv://tololkusyan:KFcY2Jz95fNlCpsu@cluster0.fn5jrbr.mongodb.net/?retryWrites=true&w=majority//dbd
+mongodb+srv://tololkusyan:<KFcY2Jz95fNlCpsu>@cluster0.fn5jrbr.mongodb.net/?retryWrites=true&w=majority//dbd

bdb19e5caa2806d4d6eb6a6388c584fe9ed0a4ac
defan312 <106823339+defan312@users.noreply.github.com>
2023-03-02 01:24:24 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'Defan'// yourname
-global.MONGO_DB_URI = "mongodb+srv://tololkusyan:KFcY2Jz95fNlCpsu@cluster0.fn5jrbr.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.MONGO_DB_URI = "database/db." //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "anabotmd@gail.com" //email
 global.email_password = "k0y4nt0@" //application password email
